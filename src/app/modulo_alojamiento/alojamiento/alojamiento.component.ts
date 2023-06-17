fa3f9f34c99b8a9bf59c2adeb0b476e23126462a
Ivette Zamora <89620932+Ivetteza21@users.noreply.github.com>
2023-06-17 14:54:45 -0500

    Add files via upload

---
+++ src/app/modulo_alojamiento/alojamiento/alojamiento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alojamiento',
+  templateUrl: './alojamiento.component.html',
+  styleUrls: ['./alojamiento.component.css']
+})
+export class AlojamientoComponent {
+
+}
