6cd70589ce7d6d5924f69f3d16e2a4d6bc019e9a
mariazhingri <mariazhingripaz@outlook.com>
2023-06-28 13:27:35 -0500

    media query de modulo de vuelos

--- src/app/modulo_vuelos/busqueda-vuelos/busqueda-vuelos.component.ts
+++ src/app/modulo_vuelos/busqueda-vuelos/busqueda-vuelos.component.ts
@@ -6,6 +6,7 @@ import { PersonasYClaseComponent } from '../personas-y-clase/personas-y-clase.co
 import { MatDialogRef } from '@angular/material/dialog';
 import { ListaVuelosComponent } from '../lista-vuelos/lista-vuelos.component';
 import { FormaDePagoComponent } from 'src/app/forma-de-pago/forma-de-pago.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -20,6 +21,9 @@ export class BusquedaVuelosComponent implements OnInit{
   //soloida:any
   //idayvuelta:any;
   //multidestino: any
+  intercambiar: FormGroup;
+  intercambiar_solo_ida: FormGroup;
+  intercambiar_multidestino: FormGroup;
   
   idayvuelta = true;
   soloida = false;
@@ -44,10 +48,31 @@ export class BusquedaVuelosComponent implements OnInit{
     this.lista_vuelos = false;
   }
 
-  ngOnInit(): void{
-  }
  
-  constructor(private router: Router,private dialog:MatDialog) { }
+ 
+  constructor(private router: Router,private dialog:MatDialog, private formBuilder: FormBuilder) { 
+    this.intercambiar = this.formBuilder.group({
+      input1: ['', Validators.required],
+      input2: ['', Validators.required]
+    })
+    
+    this.intercambiar = this.formBuilder.group({
+      input1: '',
+      input2: ''
+    });
+
+    this.intercambiar_solo_ida = this.formBuilder.group({
+      input1_solo_ida: '',
+      input2_solo_ida: ''
+    });
+
+    this.intercambiar_multidestino = this.formBuilder.group({
+      input1_multidestino: '',
+      input2_multidestino: ''
+    });
+
+    
+  }
 
 /*-------------------------------VENTANA EMERGENTE DE PERSONASYCLASE---------------------------------------*/
   openDialogpeersonayclase(){
@@ -76,7 +101,33 @@ export class BusquedaVuelosComponent implements OnInit{
   abrirlista_vuelos(){
     this.lista_vuelos = true;
   }
+/*-------------------------BOTON INTERCAMBIAR INFORMACIO-----*/
+ngOnInit(): void{
+  
+}
 
+intercambio_info(){
+  const temp = this.intercambiar.value.input1;
+  this.intercambiar.patchValue({
+  input1: this.intercambiar.value.input2,
+  input2: temp
+});
+}
+intercambio_info2(){
+  const tempo = this.intercambiar_solo_ida.value.input1_solo_ida;
+  this.intercambiar_solo_ida.patchValue({
+  input1_solo_ida: this.intercambiar_solo_ida.value.input2_solo_ida,
+  input2_solo_ida: tempo
+});
+}
+
+intercambio_info3(){
+  const tempos = this.intercambiar_multidestino.value.input1_multidestino;
+  this.intercambiar_multidestino.patchValue({
+  input1_multidestino: this.intercambiar_multidestino.value.input2_multidestino,
+  input2_multidestino: tempos
+});
+}
 }
 
 
